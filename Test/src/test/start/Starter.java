f18ae710ae4b804f3b3834bb078e7db8f7d04f68
Nadja Mueller <nmueller@kratos.ch.genedata.com>
2019-10-01 17:08:56 +0200

    merged stashed commit

--- Test/src/test/start/Starter.java
+++ Test/src/test/start/Starter.java
@@ -4,8 +4,8 @@ public abstract class Starter {
 
 	public static void main(String[] args) {
 		System.out.println("Starting now");
-		System.out.println("Please merge");
-		
+		System.out.println("Please merge");		
+		System.out.println("You shall not pass");
 	}
 
 }
